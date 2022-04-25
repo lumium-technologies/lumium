1c24552dc56857f7fea7fd99edd59e40c9fc2520
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-04-25 22:53:34 +0200

    Add files

---
+++ lumium-space/pages/_app.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@definitions/chakra/theme";
+import "@styles/global.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { Provider } from "react-redux";
+import store from "@redux/store";
+import { appWithTranslation } from "@i18n";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const queryClient = new QueryClient();
+  return (
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </Hydrate>
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ lumium-space/test/index.tsx
@@ -0,0 +1,43 @@
+import React, { ReactElement } from "react";
+import {
+  render as baseRender,
+  RenderOptions,
+  RenderResult,
+} from "@testing-library/react";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@definitions/chakra/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import store from "@redux/store";
+
+/**
+ * Custom renderer example with @testing-library/react
+ * You can customize it to your needs.
+ *
+ * To learn more about customizing renderer,
+ * please visit https://testing-library.com/docs/react-testing-library/setup
+ */
+
+export const AllTheProviders = ({ children }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Provider store={store}>{children}</Provider>
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
+  );
+};
+
+const render = (ui: ReactElement, options?: Omit<RenderOptions, "queries">) =>
+  baseRender(ui, { wrapper: AllTheProviders, ...options }) as RenderResult;
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { render };

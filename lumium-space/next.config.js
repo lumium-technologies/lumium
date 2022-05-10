c73eacde8ac225e15fc425f523fe48ab7640f058
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-05-10 14:07:02 +0200

    Configure webpack for web assembly

--- lumium-space/next.config.js
+++ lumium-space/next.config.js
@@ -1,3 +1,37 @@
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins([], {});
+// This entire configuration file using WasmChunksFixPlugin is because of https://github.com/vercel/next.js/issues/29362
+module.exports = withPlugins([], {
+  webpack(config, { isServer, dev }) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    if (!dev && isServer) {
+      config.output.webassemblyModuleFilename = "chunks/[id].wasm";
+      config.plugins.push(new WasmChunksFixPlugin());
+    }
+
+    return config;
+  },
+});
+
+class WasmChunksFixPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: "WasmChunksFixPlugin" },
+        (assets) =>
+          Object.entries(assets).forEach(([pathname, source]) => {
+            if (!pathname.match(/\.wasm$/)) return;
+            compilation.deleteAsset(pathname);
+
+            const name = pathname.split("/")[1];
+            const info = compilation.assetsInfo.get(pathname);
+            compilation.emitAsset(name, source, info);
+          })
+      );
+    });
+  }
+}

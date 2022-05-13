9e0ba0cc26a626e88c3f12f19f0d5989a9fd1b70
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-05-14 01:04:47 +0200

    Render markdown to v-DOM managed by seed

--- lumium-space/src/components/renderer/LumiumRenderer.tsx
+++ lumium-space/src/components/renderer/LumiumRenderer.tsx
@@ -1,14 +1,10 @@
 import axios from 'axios';
-import { render_markdown } from 'lumium-renderer';
+import { render_page } from 'lumium-renderer';
 import { useEffect, useState } from 'react';
 
 export const LumiumRenderer = () => {
-    const [page, setPage] = useState<string>("");
-
     useEffect(() => {
-        axios.get("https://raw.githubusercontent.com/D3PSI/cs-wiki/master/markdown/Formulary%20%26%20Course%20Materials%20677618886e9347f58deaf01d94bc40d7/Probability%20Theory%20db98499109bd4c6f934431bfc46e9c17.md").then((res) => {
-            setPage(render_markdown(res.data));
-        });
+        render_page();
     }, []);
-    return <div dangerouslySetInnerHTML={{__html: page}} />;
+    return null;
 };

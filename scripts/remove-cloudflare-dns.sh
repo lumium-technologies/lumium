22028ea2cb6cce84e137368c29624f21a1d8e059
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-05-26 14:55:37 +0200

    Fix cloudflare integration

--- scripts/remove-cloudflare-dns.sh
+++ scripts/remove-cloudflare-dns.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-record_id = $(cat cloudflare-record.json | grep "\"id\":" | awk '{print $2}' | tr -d '"' | tr -d ',')
+record_id=$(cat cloudflare-record.json | jq '.result.id' | tr -d '"')
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/32237bfe24f8077cafdfe053c67c103f/dns_records/${record_id}" \
-    -H "X-Auth-Email: ${CLOUDFLARE_API_EMAIL}" \
-    -H "X-Auth-Key: ${CLOUDFLARE_API_TOKEN}" \
+    -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
     -H "Content-Type: application/json"

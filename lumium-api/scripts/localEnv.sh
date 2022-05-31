5e01c21c1c4d0327cc2d1ffe46b5fa8cbdcc788d
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-05-31 21:04:40 +0200

    Accommodate non-admin members

--- lumium-api/scripts/localEnv.sh
+++ lumium-api/scripts/localEnv.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-export SUPERTOKENS_API_KEY=$(heroku config -a lumium-staging-api | grep SUPERTOKENS_API_KEY | awk '{ print $2 }')
-export SUPERTOKENS_CONNECTION_URI=$(heroku config -a lumium-staging-api | grep SUPERTOKENS_CONNECTION_URI | awk '{ print $2 }')
-export DATABASE_URL="postgres://development:development@localhost:5432/lumium"
+if [ -z $LUMIUM_COLLABORATOR ]; then
+    echo "Pulling environment from staging..."
+    export SUPERTOKENS_API_KEY=$(heroku config -a lumium-staging-api | grep SUPERTOKENS_API_KEY | awk '{ print $2 }')
+    export SUPERTOKENS_CONNECTION_URI=$(heroku config -a lumium-staging-api | grep SUPERTOKENS_CONNECTION_URI | awk '{ print $2 }')
+    export DATABASE_URL="postgres://development:development@localhost:5432/lumium"
+fi

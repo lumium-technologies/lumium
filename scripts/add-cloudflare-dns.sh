2be7db16c4474ce04d6c8624145655c12b52c641
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-06-01 14:36:46 +0200

    Fix predestroy.js

--- scripts/add-cloudflare-dns.sh
+++ scripts/add-cloudflare-dns.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 set -e
-if [[ -f cloudflare-record.json ]]; then
-    ./scripts/remove-cloudflare-dns.sh
-fi
 if [[ ${LUMIUM_COMPONENT} == "lumium-api" ]]; then
     curl -X POST "https://api.cloudflare.com/client/v4/zones/32237bfe24f8077cafdfe053c67c103f/dns_records" \
         -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
         -H "Content-Type: application/json" \
-        --data "{\"type\":\"CNAME\",\"name\":\"pr-${HEROKU_PR_NUMBER}.api.review.lumium.space\",\"content\":\"$1\",\"ttl\":60,\"proxied\":true}" \
-        -o cloudflare-record.json
+        --data "{\"type\":\"CNAME\",\"name\":\"pr-${HEROKU_PR_NUMBER}.api.review.lumium.space\",\"content\":\"$1\",\"ttl\":60,\"proxied\":true}"
 
 elif [[ ${LUMIUM_COMPONENT} == "lumium-space" ]]; then
     curl -X POST "https://api.cloudflare.com/client/v4/zones/32237bfe24f8077cafdfe053c67c103f/dns_records" \
         -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
         -H "Content-Type: application/json" \
-        --data "{\"type\":\"CNAME\",\"name\":\"pr-${HEROKU_PR_NUMBER}.review.lumium.space\",\"content\":\"$1\",\"ttl\":60,\"proxied\":true}" \
-        -o cloudflare-record.json
+        --data "{\"type\":\"CNAME\",\"name\":\"pr-${HEROKU_PR_NUMBER}.review.lumium.space\",\"content\":\"$1\",\"ttl\":60,\"proxied\":true}"
 fi

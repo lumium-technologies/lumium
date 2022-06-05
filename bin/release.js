b80354e195ed3cb8218698822fb24f90da3a6643
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-06-05 19:07:35 +0200

    Only copy database in review apps

--- bin/release.js
+++ bin/release.js
@@ -7,8 +7,10 @@ console.log("Done running release phase");
 async function run() {
     const appName = process.env.HEROKU_APP_NAME;
     if (process.env.LUMIUM_COMPONENT === 'lumium-api'){
-        if (execSync(`PGSSLMODE=require heroku pg:psql -c \"SELECT EXISTS ( SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'typeorm_metadata' );\" -a ${appName} | grep -B1 \"(1 row)\"`).includes("f")) {
-            execSync(`heroku pg:copy --confirm=${appName} lumium-staging-api::DATABASE DATABASE -a ${appName}`);
+        if (process.env.REVIEW_APP) {
+            if (execSync(`PGSSLMODE=require heroku pg:psql -c \"SELECT EXISTS ( SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'typeorm_metadata' );\" -a ${appName} | grep -B1 \"(1 row)\"`).includes("f")) {
+                execSync(`heroku pg:copy --confirm=${appName} lumium-staging-api::DATABASE DATABASE -a ${appName}`);
+            }
         }
         const output = execSync("npm run db:migrate");
         console.log("Running database migrations..., Output: ", output.toString());

ebded2d2f2667c8b101b33146e9936e0f10ebdb1
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-06-05 16:04:06 +0200

    Move initial database setup to release phase to ensure we are not running any migrations before having the staging database imported

--- bin/release.js
+++ bin/release.js
@@ -7,6 +7,9 @@ console.log("Done running release phase");
 async function run() {
     const appName = process.env.HEROKU_APP_NAME;
     if (process.env.LUMIUM_COMPONENT === 'lumium-api'){
+        if (execSync(`PGSSLMODE=require heroku pg:psql -c \"SELECT EXISTS ( SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'typeorm_metadata' );\" -a ${appName} | grep -B1 \"(1 row)\"`).includes("f")) {
+            execSync(`heroku pg:copy lumium-staging-api::DATABASE DATABASE -a ${appName}`);
+        }
         const output = execSync("npm run db:migrate");
         console.log("Running database migrations..., Output: ", output);
     }

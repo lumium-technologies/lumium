bfd1b8d0dda95f7a8a3c9b363d165746dad53dc0
ReddixT <rene_ottenburg@icloud.com>
2022-06-06 20:33:36 +0200

    refactor: remove failing boilerplate code

--- lumium-dashboard/pages/index.tsx
+++ lumium-dashboard/pages/index.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 import { Spacer, Flex } from "@chakra-ui/react";
 
-import { Header, Main, Cards, Footer } from "@components";
-
 const Home: React.FC = () => {
-  return (
-    <Flex direction="column" minH="100vh">
-      <Header />
-      <Main />
-      <Cards />
-      <Spacer />
-      <Footer />
-    </Flex>
-  );
+  return null;
 };
 
 export default Home;
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 if [[ ! -z ${REVIEW_APP} ]]; then
     cp lumium-space/.env.review lumium-space/.env
     cp lumium-api/.env.review lumium-api/.env
+    cp lumium-dashboard/.env.review lumium-dashboard/.env
 fi
 if [[ -z ${LUMIUM_COMPONENT} ]]; then
     npm run build --workspaces

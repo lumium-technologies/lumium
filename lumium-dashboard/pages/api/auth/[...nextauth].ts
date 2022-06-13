894feb31eab85c01ba7b654eef53dcd315006668
ReddixT <rene_ottenburg@icloud.com>
2022-06-13 13:50:39 +0200

    feature: start implementing heroku oauth

---
+++ lumium-dashboard/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export default NextAuth({
+    providers: [
+        {
+            id: "heroku",
+            name: "Heroku",
+            type: "oauth",
+            authorization: "https://id.heroku.com/oauth/authorize",
+            token: "https://id.heroku.com/oauth/token",
+            clientId: process.env.HEROKU_OAUTH_ID,
+        }
+    ],
+    secret: process.env.SECRET,
+
+    session: {
+        strategy: 'jwt'
+    },
+    jwt: {
+        secret: process.env.SECRET,
+    },
+    pages: {
+        // signIn: '/auth/signin',  // Displays signin buttons
+        // signOut: '/auth/signout', // Displays form with sign out button
+        // error: '/auth/error', // Error code passed in query string as ?error=
+        // verifyRequest: '/auth/verify-request', // Used for check email page
+        // newUser: null // If set, new users will be directed here on first sign in
+    },
+    callbacks: {
+        // async signIn({ user, account, profile, email, credentials }) { return true },
+        // async redirect({ url, baseUrl }) { return baseUrl },
+        // async session({ session, token, user }) { return session },
+        // async jwt({ token, user, account, profile, isNewUser }) { return token }
+    },
+
+    events: {},
+
+    debug: false,
+})

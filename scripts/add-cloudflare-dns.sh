9240f2363509d38936b3b82e7c825b8de8895f84
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-06-17 14:01:07 +0200

    cloudflare: enable proxy for review apps

--- scripts/add-cloudflare-dns.sh
+++ scripts/add-cloudflare-dns.sh
@@ -5,11 +5,11 @@ if [[ ${LUMIUM_COMPONENT} == "lumium-api" ]]; then
     curl -X POST "https://api.cloudflare.com/client/v4/zones/32237bfe24f8077cafdfe053c67c103f/dns_records" \
         -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
         -H "Content-Type: application/json" \
-        --data "{\"type\":\"CNAME\",\"name\":\"pr-${HEROKU_PR_NUMBER}.api.review.lumium.space\",\"content\":\"$1\",\"ttl\":60,\"proxied\":false}"
+        --data "{\"type\":\"CNAME\",\"name\":\"pr-${HEROKU_PR_NUMBER}.api.review.lumium.space\",\"content\":\"$1\",\"ttl\":60,\"proxied\":true}"
 
 elif [[ ${LUMIUM_COMPONENT} == "lumium-space" ]]; then
     curl -X POST "https://api.cloudflare.com/client/v4/zones/32237bfe24f8077cafdfe053c67c103f/dns_records" \
         -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
         -H "Content-Type: application/json" \
-        --data "{\"type\":\"CNAME\",\"name\":\"pr-${HEROKU_PR_NUMBER}.review.lumium.space\",\"content\":\"$1\",\"ttl\":60,\"proxied\":false}"
+        --data "{\"type\":\"CNAME\",\"name\":\"pr-${HEROKU_PR_NUMBER}.review.lumium.space\",\"content\":\"$1\",\"ttl\":60,\"proxied\":true}"
 fi

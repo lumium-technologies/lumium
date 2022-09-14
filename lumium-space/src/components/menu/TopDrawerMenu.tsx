976b469b54db755f276e3781d05de98198ee84aa
ReddixT <rene_ottenburg@icloud.com>
2022-09-14 12:06:13 +0200

    feat: edited ui when logged in changed top menu to sidebar added profile with logout added font size ui

--- lumium-space/src/components/menu/TopDrawerMenu.tsx
+++ lumium-space/src/components/menu/TopDrawerMenu.tsx
@@ -1,26 +1,98 @@
-import { Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, useDisclosure, IconButton } from '@chakra-ui/react';
-import { ArrowDownIcon } from '@chakra-ui/icons';
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    Input,
+    IconButton,
+    Flex,
+    Spacer,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+    Button,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
+} from '@chakra-ui/react'
+import { ArrowRightIcon } from '@chakra-ui/icons';
+import { signOut } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
 import React from 'react';
+
+async function onLogout() {
+    await signOut();
+    window.location.href = "/";
+}
 
 function TopDrawerMenu() {
     const { isOpen, onOpen, onClose } = useDisclosure()
 
     return (
         <>
-            <IconButton aria-label="open menu" icon={<ArrowDownIcon />} onClick={onOpen}>
-                Open
-            </IconButton>
-            <Drawer placement={"top"} onClose={onClose} isOpen={isOpen}>
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerHeader borderBottomWidth='1px'>Pages</DrawerHeader>
-                    <DrawerBody>
-                        <p>Some page</p>
-                        <p>Another page</p>
-                        <p>More pages...</p>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
+            <Flex>
+                <IconButton onClick={onOpen} aria-label={''} icon={<ArrowRightIcon />} >
+                    Open
+                </IconButton>
+                <Drawer
+                    isOpen={isOpen}
+                    placement='left'
+                    onClose={onClose}
+                >
+                    <DrawerOverlay />
+                    <DrawerContent>
+                        <DrawerCloseButton />
+                        <DrawerHeader>Pages</DrawerHeader>
+
+                        <DrawerBody>
+                            Some pages
+                            <Button display="flex" position="absolute" bottom="2%">
+                                + Add new page
+                            </Button>
+                        </DrawerBody>
+
+                        <DrawerFooter>
+
+                        </DrawerFooter>
+                    </DrawerContent>
+                </Drawer>
+
+                <NumberInput defaultValue={11} min={1} max={99} width="4%">
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+
+                <Spacer />
+
+                <Menu>
+                    <MenuButton as={Button} backgroundColor={'darkgrey'}>
+                    </MenuButton>
+                    <MenuList>
+                        <MenuGroup title='Profile'>
+                            <MenuItem>My Account</MenuItem>
+                            <MenuItem onClick={onLogout}>Logout</MenuItem>
+                        </MenuGroup>
+                        <MenuDivider />
+                        <MenuGroup title='Help'>
+                            <MenuItem>Docs</MenuItem>
+                            <MenuItem>FAQ</MenuItem>
+                        </MenuGroup>
+                    </MenuList>
+                </Menu>
+            </Flex>
         </>
     )
 }

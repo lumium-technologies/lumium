b4d59e82d745234ce91e1ab93f7f0b67315ddf49
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-10-04 11:30:43 +0200

    feat: add conditional compilation for renderer

--- lumium-renderer/src/lib.rs
+++ lumium-renderer/src/lib.rs
@@ -3,18 +3,12 @@ mod utils;
 use katex_renderer::render_katex;
 use pulldown_cmark::{html, Options, Parser};
 use seed::{prelude::*, *};
+use serde::Deserialize;
 use wasm_bindgen::prelude::*;
+use web_sys::Request;
 
 extern crate web_sys;
 
-// A macro to provide `println!(..)`-style syntax for `console.log` logging.
-macro_rules! log {
-    ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
-}
-
-#[allow(non_snake_case)]
 #[wasm_bindgen]
 pub fn render_markdown(markdown: &str) -> String {
     let preprocessed_markdown = render_katex(markdown.to_string()).expect("Failed to render katex");
@@ -24,41 +18,56 @@ pub fn render_markdown(markdown: &str) -> String {
     html_out
 }
 
+#[derive(Deserialize, Clone, PartialEq, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ContentElement {
+    content: String,
+    r#type: String,
+}
+
+#[derive(Deserialize, Clone, PartialEq, Debug)]
+#[serde(rename_all = "camelCase")]
+struct PageContent {
+    position: u32,
+    content_element: ContentElement,
+}
+
+#[derive(Deserialize, Clone, PartialEq, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Page {
+    contents: Vec<PageContent>,
+}
+
+fn get_page_contents() -> String {
+    let url = env!("API_HOST");
+    //let req = Request::new(url);
+    "".to_string()
+}
+
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
     utils::set_panic_hook();
-    Model {
-        page: "# Probability Theory
-# Definitions and Basic Terms
-<aside>
-💡 A *discrete probability space* is defined by the *sample space/possibility space* $\\Omega = \\{\\omega_1, \\omega_2,...\\}$ of *elementary events*. An (elementary-) probability $\\Pr[\\omega_i]$ is assigned to every elementary event $\\omega_i$, where we require $0\\leq \\Pr[\\omega_i] \\leq 1$ and
-$$
-\\sum_{\\omega \\in \\Omega} \\Pr[\\omega]=1.
-$$
-A set $E \\sube \\Omega$ is called an *event*. The probability $\\Pr[E]$ of an event is defined by
-$$
-\\Pr[E] = \\sum_{\\omega \\in E} \\Pr[\\omega].
-$$".to_string()
-    }
+    let content = get_page_contents();
+    Model { content }
 }
 
 struct Model {
-    page: String,
+    content: String,
 }
 
 enum Msg {
-    Page(String),
+    Content(String),
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Page(md) => model.page = md,
+        Msg::Content(md) => model.content = md,
     }
 }
 
 fn view(model: &Model) -> Node<Msg> {
     div![Node::from_html(
         Some(&Namespace::Html),
-        &render_markdown(model.page.as_str()),
+        &render_markdown(model.content.as_str()),
     )]
 }
 

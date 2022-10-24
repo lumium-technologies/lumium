0be59c74902bc1878582a5f06a1afaf1d61a24a7
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-10-24 21:06:48 +0200

    ops(clang): build clang in parallel

--- lumium-space/scripts/rust-toolchain.sh
+++ lumium-space/scripts/rust-toolchain.sh
@@ -21,7 +21,7 @@ else
     mkdir build
     cd build
     cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../llvm
-    make
+    make -j`nproc`
 )
 export PATH="$PATH:$HOME/.clang/llvm/build/bin"
 fi

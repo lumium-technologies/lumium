b349b4d79d5505b5222981ffcdaaa1e3c1b29874
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-10-24 22:18:00 +0200

    ops(clang): add apt buildpack (experimental)

--- lumium-space/scripts/rust-toolchain.sh
+++ lumium-space/scripts/rust-toolchain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CLANG_VERSION="14.0.6"
-CLANG_URL="http://llvm.org/releases/$CLANG_VERSION/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-16.04.tar.xz"
+CLANG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-$CLANG_VERSION/clang+llvm-$CLANG_VERSION-aarch64-linux-gnu.tar.xz"
 
 if which cargo >/dev/null && which wasm-pack >/dev/null; then
     echo "rust toolchain already installed"
@@ -11,20 +11,4 @@ else
     curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | bash
     . "$HOME/.cargo/env"
     export PATH="$PATH:$HOME/.cargo/bin"
-fi
-if which clang >/dev/null; then
-    echo "clang already installed"
-else
-    echo "installing clang"
-    (
-    mkdir $HOME/.clang
-    cd $HOME/.clang/
-    wget --quiet $CLANG_URL
-    mkdir -p libs/clang
-    tar -C libs/clang -xvf clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-    mv libs/clang/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-16.04/* libs/clang
-    rmdir libs/clang/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-16.04
-    rm -f clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-)
-export PATH="$PATH:$HOME/.clang/libs/clang/bin"
 fi

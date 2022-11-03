4e24ded6c4e2458d4ae48e26addc1d5ba8a23b7f
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-11-03 19:25:25 +0100

    fix:build script

--- lumium-space/scripts/build-renderer.sh
+++ lumium-space/scripts/build-renderer.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 (which git >/dev/null && git submodule update --init --recursive)
-    clang_present=$(which clang)
-    if [ -x "$clang_present" ]; then
         docker run --volume $(pwd)/../:/app ubuntu /app/lumium-space/scripts/docker-entry.sh
-    else
-        . ./scripts/rust-toolchain.sh && wasm-pack build --target web lumium-renderer
-    fi
     cd lumium-renderer/pkg && yarn link && cd ../../ && yarn link lumium-renderer

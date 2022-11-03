83e15d5c76881ea3d290bf68121893127f71487c
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-11-03 19:36:34 +0100

    ops:add docker buildpack

--- lumium-space/scripts/build-renderer.sh
+++ lumium-space/scripts/build-renderer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 (which git >/dev/null && git submodule update --init --recursive)
-        docker run --volume $(pwd)/../:/app ubuntu /app/lumium-space/scripts/docker-entry.sh
-    cd lumium-renderer/pkg && yarn link && cd ../../ && yarn link lumium-renderer
+
+. ./scripts/rust-toolchain.sh && wasm-pack build lumium-renderer &&
+cd lumium-renderer/pkg && yarn link && cd ../../ && yarn link lumium-renderer

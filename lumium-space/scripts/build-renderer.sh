69f31bccd3675b83f1bcc614c2ab6b91a3d99758
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-11-06 16:58:19 +0000

    refactor:toolchain script

--- lumium-space/scripts/build-renderer.sh
+++ lumium-space/scripts/build-renderer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 (which git >/dev/null && git submodule update --init --recursive)
 
-. ./scripts/rust-toolchain.sh && wasm-pack build lumium-renderer &&
+. ./scripts/toolchain.sh && wasm-pack build lumium-renderer &&
 cd lumium-renderer/pkg && yarn link && cd ../../ && yarn link lumium-renderer
---
+++ lumium-space/scripts/toolchain.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if which cargo >/dev/null; then
+    echo "rust toolchain already installed"
+else
+    echo "installing rust toolchain"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    . "$HOME/.cargo/env"
+    export PATH="$PATH:$HOME/.cargo/bin"
+fi
+
+if which wasm-pack >/dev/null; then
+    echo "wasm toolchain already installed"
+else
+    echo "installing wasm toolchain"
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | bash
+    . "$HOME/.cargo/env"
+fi
+
+if which clang >/dev/null; then 
+    echo "clang toolchain already installed"
+else
+    echo "installing clang toolchain"
+    mkdir $HOME/.clang
+    curl https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz -L --output clang.tar.xz
+    tar -xvf clang.tar.xz 
+    mv clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04/ $HOME/.clang
+    export PATH="$PATH:$HOME/.clang/bin"
+fi

e39316ef532b2444320cfe9fbe3578c06f0a9bfe
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-11-06 17:20:26 +0000

    fix:clang install

--- lumium-space/scripts/toolchain.sh
+++ lumium-space/scripts/toolchain.sh
@@ -20,10 +20,9 @@ if which clang >/dev/null; then
     echo "clang toolchain already installed"
 else
     echo "installing clang toolchain"
-    mkdir $HOME/.clang
+    rm -rf clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04
     curl https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz -L --output clang.tar.xz
     tar -xvf clang.tar.xz 
-    mv clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04/ $HOME/.clang
     rm -rf clang.tar.xz
-    export PATH="$PATH:$HOME/.clang/bin"
+    export PATH="$PATH:$PWD/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04/bin"
 fi

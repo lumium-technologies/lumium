2108cc60dbfc1821ceb18f0be0dd6bacc0e7da27
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-11-18 22:05:25 +0100

    feat(landing):add landing page template

---
+++ lumium-space/pages/_app.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@definitions/chakra/theme";
+import "@styles/global.css";
+
+import SuperTokensReact from 'supertokens-auth-react'
+
+import { frontendConfig } from '../config/frontendConfig'
+
+if (typeof window !== 'undefined') {
+    SuperTokensReact.init(frontendConfig())
+}
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
+}
+
+export default MyApp;
---
+++ lumium-space/pages/index.tsx
@@ -0,0 +1,16 @@
+import { CallToAction } from "@sections/landing/CallToAction";
+import { Footer } from "@sections/landing/Footer";
+import { Pricing } from "@sections/landing/Pricing";
+import { Statistics } from "@sections/landing/Statistics";
+
+export default function LandingPage() {
+    return (
+        <>
+            <CallToAction />
+            <Statistics />
+            <Pricing />
+            <Footer />
+        </>
+    );
+};
+

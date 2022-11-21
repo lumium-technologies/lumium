155f7ba86afa88d6f8b7affa571735ce297817b9
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-11-21 12:04:16 +0100

    ops(docker):specify target platform

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,2 +1,2 @@
-docker build -t lumium-space:latest . -f lumium-space/Dockerfile
-docker build -t lumium-api:latest . -f lumium-api/Dockerfile
+docker build -t lumium-space:latest . -f lumium-space/Dockerfile --platform=amd64
+docker build -t lumium-api:latest . -f lumium-api/Dockerfile --platform=amd64

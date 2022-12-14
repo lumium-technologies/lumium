91c42018621004876c3792e465cfdaff08200a90
theArgex <24879428+theArgex@users.noreply.github.com>
2022-12-14 23:25:06 +0100

    feat: hero particles

--- lumium-space/src/sections/landing/CallToAction.tsx
+++ lumium-space/src/sections/landing/CallToAction.tsx
@@ -8,10 +8,107 @@ import {
     Icon,
     IconProps,
 } from '@chakra-ui/react';
+import { useCallback } from 'react';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 export function CallToAction() {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
     return (
-        <Container maxW={'5xl'}>
+        <>
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    opacity: 0,
+                    color: {
+                        value: "#1f1f1f",
+                    },
+                },
+                fullScreen: {
+                    enable: false
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: false,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: false,
+                            mode: "repulse",
+                        },
+                        resize: false,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: false,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+            />
+        <Container maxW={'5xl'} h={'100vh'}>
             <Stack
                 textAlign={'center'}
                 align={'center'}
@@ -52,6 +149,7 @@ export function CallToAction() {
                 </Flex>
             </Stack>
         </Container>
+                        </> 
     );
 }
 

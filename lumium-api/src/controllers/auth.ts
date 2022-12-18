066bcb4c8bb92496ce2d7ef781dab75de837cafd
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-12-18 17:52:30 +0100

    feat(auth):implement sign in and sign up

--- lumium-api/src/controllers/auth.ts
+++ lumium-api/src/controllers/auth.ts
@@ -4,6 +4,11 @@ import { dataSource } from '../data-source';
 import { User } from '../entity/User';
 import crypto from 'crypto';
 import { generateAccessToken } from '../cypto';
+import { Email } from '../entity/Email';
+import { AuthenticationInformation } from '../entity/AuthenticationInformation';
+
+const peppers =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export const signIn = async (req: express.Request<UserAuthDTO>, res: express.Response) => {
     const user = await dataSource.getRepository(User).findOne({
@@ -19,23 +24,36 @@ export const signIn = async (req: express.Request<UserAuthDTO>, res: express.Res
     if (!user) {
         res.status(401).send();
     }
-    let keys = Buffer.from(user.auth?.variant, 'base64');
-    Array.from('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ').forEach((pepper) => {
-        crypto.pbkdf2(
+    let keys = Buffer.from(user.auth!.key, 'base64');
+    Array.from(peppers).forEach((pepper) => {
+        const derivedKey = crypto.pbkdf2Sync(
             Buffer.from(req.body.password + pepper, 'utf8'),
             Buffer.from(process.env.SALT, 'base64'),
             100000,
             64,
-            'sha512',
-            (err, derivedKey) => {
-                if (err) {
-                    throw err;
-                }
-                if (keys.includes(derivedKey)) {
-                    res.status(200).cookie('accessToken', generateAccessToken(user.id), { httpOnly: true }).send();
-                }
-            });
+            'sha512'
+        );
+        if (keys.includes(derivedKey)) {
+            res.status(200).cookie('accessToken', generateAccessToken({ userId: user.id }), { httpOnly: true }).send();
+        }
     });
+    res.status(401).send();
 };
 
-export const signUp = async () => { };
+export const signUp = async (req: express.Request<UserAuthDTO>, res: express.Response) => {
+    const pepper = peppers[Math.floor(Math.random() * 52)];
+    const derivedKey = crypto.pbkdf2Sync(
+        Buffer.from(req.body.password + pepper, 'utf8'),
+        Buffer.from(process.env.SALT, 'base64'),
+        100000,
+        64,
+        'sha512'
+    );
+    let user = await dataSource.getRepository(User).save({});
+    let email = await dataSource.getRepository(Email).save({ user, primary: true, email: req.body.email! });
+    let auth = new AuthenticationInformation();
+    auth.key = derivedKey.toString('base64');
+    user.auth = auth;
+    await dataSource.getRepository(User).save(user);
+    res.status(200).cookie('accessToken', generateAccessToken({ userId: user.id }), { httpOnly: true }).send();
+};
--- routes/api/v1/public/index.ts
+++ routes/api/v1/public/index.ts
@@ -2,5 +2,7 @@ import { postSignIn, postSignUp } from './auth';
 
 export const PING = '/ping';
 export const AUTH = '/auth';
-export const AUTH_SIGNIN_POST = AUTH + postSignIn;
-export const AUTH_SIGNUP_POST = AUTH + postSignUp;
+export const SIGNIN_POST = postSignIn;
+export const SIGNUP_POST = postSignUp;
+export const AUTH_SIGNIN_POST = AUTH + SIGNIN_POST;
+export const AUTH_SIGNUP_POST = AUTH + SIGNUP_POST;

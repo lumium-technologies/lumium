afe876aabb849c02897c1fdde3e551eeb287dc7d
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-12-24 19:03:28 +0100

    feat(space):add transitions

--- lumium-space/pages/_app.tsx
+++ lumium-space/pages/_app.tsx
@@ -3,11 +3,14 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@definitions/chakra/theme";
 import "@styles/global.css";
+import PageTransition from "@components/transitions/PageTransition";
 
 const Lumium = ({ Component, pageProps }: AppProps): JSX.Element => {
     return (
         <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+            <PageTransition>
+                <Component {...pageProps} />
+            </PageTransition>
         </ChakraProvider>
     );
 };
